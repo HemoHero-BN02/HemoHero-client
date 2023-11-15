5e1ba8503d04ee40e769300bed2076790c15d7bc
LeeYeonJae <lyeonj@sookmyung.ac.kr>
2023-11-15 23:57:55 +0900

    [연재]Login.js 생성(11/15)

--- hemohero/src/App.js
+++ hemohero/src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
 //라우팅 페이지
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
