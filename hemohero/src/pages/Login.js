6f33ec1b6df2e8f4ac2ea6afdb3285b7e86f7a9b
LeeYeonJae <lyeonj@sookmyung.ac.kr>
2023-11-17 02:48:24 +0900

    [연재] Login.js & Login.css 수정(11/17)

--- hemohero/src/pages/Login.js
+++ hemohero/src/pages/Login.js
@@ -4,7 +4,27 @@ import "../css/Login.css";
 function Login(props) {
   return (
     <div>
-      <p id="login_title">안녕ㅎㅎ</p>
+      <img src={process.env.PUBLIC_URL + '/images/logo.png'} alt="HemoHero_logo" id="login_logo"></img>
+      <p id="login_title">로그인</p>
+
+      <div className="login_input">
+        <img src={process.env.PUBLIC_URL + '/images/id_icon.png'} id="login_idicon"></img>
+        <input type="text" id="login_id" placeholder="아이디"></input>
+      </div>
+
+      <div className="login_input">
+        <img src={process.env.PUBLIC_URL + '/images/pw_icon.png'} id="login_pwicon"></img>
+        <input type="password" id="login_pw" placeholder="비밀번호"></input>
+      </div>
+
+      <div className="login_save">
+        <input type="checkbox" id="login_savecheck"></input>
+        <p id="login_idsave">아이디 저장</p>
+      </div>
+
+      <input type="button" id="login_click" value="로그인"></input>
+
+      <p id="login_join">회원가입</p>
     </div>
   );
 }
